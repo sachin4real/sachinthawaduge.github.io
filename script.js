1046f89dd22de477762dca6b3e88b1187f4ebb2e
sachin4real <105566782+sachin4real@users.noreply.github.com>
2023-12-12 23:58:28 +0530

    change

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("contact-form").addEventListener("submit", function (event) {
-    event.preventDefault();
-    // Add your code to handle the form submission (e.g., sending data to a server)
-    alert("Form submitted!");
-});
-
