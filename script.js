8ec194affa4c09c88f7615610378d191cc387474
sachin4real <105566782+sachin4real@users.noreply.github.com>
2023-12-12 21:10:41 +0530

    Pilot
    html,css,js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Add your code to handle the form submission (e.g., sending data to a server)
+    alert("Form submitted!");
+});
+
